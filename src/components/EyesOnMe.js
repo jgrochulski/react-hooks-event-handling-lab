94bde942a61cbb46f52f70f374505d7d16d88528
jan <janek.grochulski@gmail.com>
2021-11-30 10:19:47 -0500

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+      return (
+            <button 
+                  // onClick={() => console.log('clicked')}
+                  onFocus={() => console.log('Good!')}
+                  onBlur={() => console.log('Hey! Eyes on me!')}>
+                  Eyes on me</button>
+      );
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+
+function Keypad() {
+      return (
+      //      <input type="password" onChange={(event) => console.log(event.target.value)} />
+           <input type="password" onChange={(event) => console.log('Entering password...')} />
+
+
+      );
+};
+
+export default Keypad
